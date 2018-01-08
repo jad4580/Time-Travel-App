a7c06658d138940c0943295fab4a124c826ff5eb
jad4580 <jdoaner98@yahoo.com>
2018-01-08 14:34:26 -0500

    Initial Commit

---
+++ bttf/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  bttf
+//
+//  Created by Justin Doan on 1/8/18.
+//  Copyright © 2018 Justin Doan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
